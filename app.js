8321e41cbaf7aa2ff6db2942d0855bae03303633
Solomon Patricia <solomonpatricia333@gmail.com>
2025-11-30 20:58:47 +0200

    Am refacut html si am reusit sa fac modalul de add notes mai trebuie lucrat in css pentru container-cards si de facut functionalitatiile in java

--- app.js
+++ app.js
@@ -1,3 +1,1 @@
-
-createarticle("Test2","25.11.2025","test2");
-createarticle("test3","25.11.2025","test3")
+loadArticles(data);
--- functions.js
+++ functions.js
@@ -1,15 +1,15 @@
 
-function createarticle (title,date,desc){
-let articlescontainer= document.querySelector('.grid-article');
+function createarticle (note){
 
     let article = document.createElement("article");
+
     article.innerHTML=`
           <div class="header-book">
-          <p class="article-title">${title}</p>
-          <p class="date-article">${date}</p>
+          <p class="article-title">${note.title}</p>
+          <p class="date-article">${note.date}</p>
         </div>
 
-        <div class="desc-article">${desc}
+        <div class="desc-article">${note.desc}
         </div>
 
         <div class="footer-article btn-container">
@@ -18,12 +18,26 @@ let articlescontainer= document.querySelector('.grid-article');
         </div>
     
     `
-
-    articlescontainer.appendChild(article);
-    article.classList.add('box-article');
+    article.classList.add("cards-container");
+    article.classList.add("card");
+    return article;
 
 }
 
+function loadArticles(articles){
+
+  let list = document.querySelector(".cards-container");
+
+  list.innerHTML="";
+
+  articles.map((article)=> createarticle(article)).forEach(element => {
+    
+      list.appendChild(element);
+
+  });
+
+
+}
 
 
 
